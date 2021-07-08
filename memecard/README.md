70762dba3e2cc45119247c6a1b1163b94fcb38c8
Himanshu Agarwal <55655064+himanshuagarwal407@users.noreply.github.com>
2021-07-08 23:54:56 +0530

    Add files via upload

---
+++ memecard/README.md
@@ -0,0 +1,1 @@
+Memecard app
---
+++ memecard/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css"
+function App() {
+  return (
+    <div className = "main">
+    <div className = "card">
+      <img src  = "meme.jpeg" alt = "virat meme"/>
+      <h3>React Card</h3>
+      </div>
+      </div>
+  );
+}
+
+export default App;
