ef48c91e13f09aa1126bb139f7ff58891193bdce
Himanshu Agarwal <55655064+himanshuagarwal407@users.noreply.github.com>
2021-07-08 21:31:51 +0530

    Delete Day16/chess-board/chess-board directory

--- Day16/chess-board/chess-board/src/Box.js
+++
@@ -1,9 +0,0 @@
-import	React from 'react'
-import "./Box.css"
-function Box({color}) 
-    {
-        return (
-            <div className="box" style ={{backgroundColor : color, border:"black"}}>
-             </div>)
-    }
-export default Box
